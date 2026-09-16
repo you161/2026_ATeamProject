7f4359e43bae6a03d385f59a9cbfa3833dcdc69b
you <kamadayou10@gmail.com>
2026-09-16 16:30:42 +0900

    [Update] PG_15 プレイヤーの影処理を更新

--- 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Player/PlayerShadowTracking.cs
+++ 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Player/PlayerShadowTracking.cs
@@ -4,31 +4,25 @@ public class PlayerShadowTracking : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
     [SerializeField] private GameObject shadowObject = null;
-    private bool isFalling = false;
-    public bool IsFalling { get => isFalling; }
-
+    [SerializeField] private float shadowOffset = 0;
     private void Update()
     {
         //影の位置を更新
         if (Physics.Raycast(rb.position, Vector3.down, out RaycastHit hit))
         {
-            shadowObject.transform.position = hit.point;
-
-            if (isFalling)
+            if (!shadowObject.activeSelf)
             {
-                isFalling = false;
-                rb.useGravity = false;
+                shadowObject.SetActive(true);
             }
+
+            shadowObject.transform.position = hit.point + Vector3.up * shadowOffset;
         }
         else
         {
-            if (!isFalling)
+            if (shadowObject.activeSelf)
             {
-                isFalling = true;
-                rb.useGravity = true;
+                shadowObject.SetActive(false);
             }
-
-            shadowObject.transform.position = rb.position + Vector3.down * 5.0f;
         }
     }
 }
