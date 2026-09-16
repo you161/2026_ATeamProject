d0d495573e8cfb561784c08d04f75beee19a6fec
you <kamadayou10@gmail.com>
2026-09-16 13:54:40 +0900

    [Add] PG_11 タイトル用スクリプトの追加

---
+++ 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Title/TitleManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] private int maxPlayerCount = 2;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
