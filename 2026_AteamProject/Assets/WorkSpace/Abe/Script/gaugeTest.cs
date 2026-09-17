d178e7f376afb97ef8e0ccad980227c0235afa2a
Raeb <syun.jstw@gmail.com>
2026-09-17 09:50:42 +0900

    [Add]PG_22 爆弾のクールタイムのゲージの追加

---
+++ 2026_AteamProject/Assets/WorkSpace/Abe/Script/gaugeTest.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gaugeTest : MonoBehaviour
+{
+    [SerializeField] private Image gaugeImage = null;
+    [SerializeField] private float coolDownTime = 0.0f;
+
+    public float countTime = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        countTime += Time.deltaTime;
+        gaugeImage.fillAmount -= 1.0f / coolDownTime * Time.deltaTime;
+
+        if (countTime >= coolDownTime) countTime = coolDownTime;
+    }
+}
