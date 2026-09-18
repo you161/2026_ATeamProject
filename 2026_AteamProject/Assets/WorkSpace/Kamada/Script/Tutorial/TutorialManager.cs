32e444ea2950cd90cef639b7ae2acd52fa23baa2
you <kamadayou10@gmail.com>
2026-09-18 11:39:11 +0900

    [Update] PG_34 チュートリアル処理の更新

--- 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Tutorial/TutorialManager.cs
+++ 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Tutorial/TutorialManager.cs
@@ -6,23 +6,60 @@ public class TutorialManager : MonoBehaviour
 {
     [SerializeField] private Image tutorialImage = null;
     [SerializeField] private Sprite[] tutorialSprites = null;
+    [SerializeField] private GameObject controllerUI = null;
+    [SerializeField] private GameObject keyboardUI = null;
+    [SerializeField] private GameSceneManager gameSceneManager = null;
     private int currentPage = 0;
+    private bool isSceneChange = false;
+    private bool hasController = false;
     private void Start()
     {
         currentPage = 0;
         tutorialImage.sprite = tutorialSprites[currentPage];
+        isSceneChange = false;
+        hasController = false;
+
+        hasController = Gamepad.all.Count > 0;
+
+        Debug.Log(hasController);
+
+        if (hasController)
+        {
+            controllerUI.SetActive(true);
+            keyboardUI.SetActive(false);
+        }
+        else
+        {
+            controllerUI.SetActive(true);
+            keyboardUI.SetActive(false);
+        }
     }
 
     private void Update()
     {
-        if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        if (hasController)
         {
-            NextPage();
+            if (Gamepad.current.aButton.wasPressedThisFrame)
+            {
+                NextPage();
+            }
 
+            if (Gamepad.current.bButton.wasPressedThisFrame)
+            {
+                PreviousPage();
+            }
         }
-        else if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        else
         {
-            PreviousPage();
+            if (Keyboard.current.dKey.wasPressedThisFrame)
+            {
+                NextPage();
+            }
+
+            if (Keyboard.current.aKey.wasPressedThisFrame)
+            {
+                PreviousPage();
+            }
         }
     }
     private void NextPage()
@@ -37,6 +74,16 @@ public class TutorialManager : MonoBehaviour
 
     private void ChangePage(int pageNum)
     {
+        if (pageNum >= tutorialSprites.Length)
+        {
+            if (!isSceneChange)
+            {
+                isSceneChange = true;
+                gameSceneManager.LoadMainScene();
+            }
+            return;
+        }
+
         if (pageNum >= 0 && pageNum < tutorialSprites.Length)
         {
             currentPage = pageNum;
