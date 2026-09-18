6da4a4af5b375567b36098ba8419efc77886fa05
you <kamadayou10@gmail.com>
2026-09-18 10:19:55 +0900

    [Update] PG_30 プレイヤーデータの更新

--- 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Player/PlayerData.cs
+++ 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Player/PlayerData.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Scriptable Objects/PlayerData")]
 public class PlayerData : ScriptableObject
 {
+    [Header("移動")]
     [Header("最高移動速度")]
     public float MaxMoveSpeed = 0;
     [Header("加速度")]
     public float Acceleration = 0;
     [Header("減速度")]
     public float Deceleration = 0;
+    [Header("回転速度")]
+    public float RotationSpeed = 0;
+
+    [Header("ジャンプ")]
     [Header("ジャンプ力")]
     public float JumpPower = 0;
     [Header("重力")]
@@ -19,12 +24,16 @@ public class PlayerData : ScriptableObject
     public float FrontJumpUpPower = 0;
     [Header("前方向のジャンプ時間")]
     public float FrontJumpTime = 0;
-    [Header("回転速度")]
-    public float RotationSpeed = 0;
+    [Header("前ジャンプ中の移動速度倍率")]
+    public float frontJumpingMoveRate = 0;
+
+    [Header("ノックバック")]
     [Header("ノックバック距離")]
     public float KnockbackDistance = 0;
     [Header("ノックバック力")]
     public float KnockbackPower = 0;
     [Header("ノックバック時間")]
     public float KnockbackTime = 0;
+    [Header("ノックバック中の移動速度倍率")]
+    public float knockbackMoveRate = 0;
 }
