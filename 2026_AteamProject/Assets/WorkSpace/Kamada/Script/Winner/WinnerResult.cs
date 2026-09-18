7efb7e6c7ddb38e45369adaf98c8d1e7bb9d37fd
you <kamadayou10@gmail.com>
2026-09-18 18:18:28 +0900

    [Update] PG_47 勝者判定処理の更新

--- 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Winner/WinnerResult.cs
+++ 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Winner/WinnerResult.cs
@@ -5,17 +5,31 @@ public class WinnerResult : MonoBehaviour
     [SerializeField] private WinnerData winnerData = null;
     [SerializeField] private GameObject[] playerObject = null;
     [SerializeField] private CameraMover cameraMover = null;
+    private Coroutine resultCoroutine;
+    private int winnerNumber = 0;
     private void Start()
     {
-        for(int i = 0; i < playerObject.Length; i++)
+        resultCoroutine = null;
+
+        for (int i = 0; i < playerObject.Length; i++)
         {
             if(i == winnerData.WinnerNumber)
             {
                 playerObject[i].SetActive(true);
-                StartCoroutine(cameraMover.ResultEffects(i));
+                winnerNumber = i;
                 continue;
             }
             playerObject[i].SetActive(false);
+        }
+
+        if (resultCoroutine != null)
+        {
+            return;
+        }
+        else
+        {
+            resultCoroutine = StartCoroutine(cameraMover.ResultEffects(winnerNumber));
+            Debug.Log("a");
         }
     }
 }
