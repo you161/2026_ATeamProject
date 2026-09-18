ec63d305464ab4e653ace85452f0b8a89aea0356
you <kamadayou10@gmail.com>
2026-09-18 11:38:57 +0900

    [Update] PG_34 タイトル処理の更新

--- 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Title/TitleManager.cs
+++ 2026_AteamProject/Assets/WorkSpace/Kamada/Script/Title/TitleManager.cs
@@ -1,16 +1,63 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TitleManager : MonoBehaviour
 {
-    [SerializeField] private int maxPlayerCount = 2;
+    [SerializeField] private GameObject controllerUI = null;
+    [SerializeField] private GameObject keyboardUI = null;
+    [SerializeField] private GameSceneManager gameSceneManager = null;
 
+    private bool hasController = false;
+    private bool isQuitting = false;
     private void Start()
     {
-        
+        hasController = false;
+        isQuitting = false;
+
+        hasController = Gamepad.all.Count > 0;
+
+        Debug.Log(hasController);
+
+        //if (hasController)
+        //{
+        //    controllerUI.SetActive(true);
+        //    keyboardUI.SetActive(false);
+        //}
+        //else
+        //{
+        //    controllerUI.SetActive(true);
+        //    keyboardUI.SetActive(false);
+        //}
     }
 
     private void Update()
     {
-        
+        //if (hasController)
+        //{
+        //    if (Gamepad.current.aButton.wasPressedThisFrame)
+        //    {
+        //        gameSceneManager.LoadMainScene();
+        //    }
+
+        //    if (!isQuitting && Gamepad.current.bButton.wasPressedThisFrame)
+        //    {
+        //        isQuitting = true;
+        //        Application.Quit();
+        //    }
+        //}
+        //else
+        //{
+        //    if (Keyboard.current.leftShiftKey.wasPressedThisFrame)
+        //    {
+        //        gameSceneManager.LoadMainScene();
+        //    }
+
+        //    if (!isQuitting && Keyboard.current.escapeKey.wasPressedThisFrame)
+        //    {
+        //        isQuitting = true;
+        //        Application.Quit();
+        //    }
+        //}
     }
 }
